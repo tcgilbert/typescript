cd2c03761970046679f316abef2ea6672e246809
Thomas Gilbert <tcgilbert94@gmail.com>
2021-03-06 17:22:48 -0500

    add other files

---
+++ test.js
@@ -0,0 +1,18 @@
+function test(obj) {
+    let copy = obj;
+    return obj;
+}
+
+let input = {
+    value: 3,
+};
+
+console.log(test(input));
+
+// function testTwo(num) {
+//     let copy = num;
+//     copy = 10;
+//     return num;
+// }
+
+// console.log(testTwo(1));
